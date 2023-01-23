ecae9be3a586817ab50cc65f836dcb5392bfb5fa
NikaN-A <nika377344@gmail.com>
2023-01-23 19:10:06 +0200

    Weather icons

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherInfo(props) {
   return (
@@ -17,7 +18,9 @@ export default function WeatherInfo(props) {
       </div>
       <div className="row">
         <div className="col-6">
-          <img src={props.data.iconUrl} className="bigemoji" id="icon" alt="" />
+          <div className="bigemoji" id="icon" alt="">
+            <WeatherIcon code={props.data.icon} />
+          </div>
           <span id="temperature" className="temperature">
             {Math.round(props.data.temperature)}
           </span>
