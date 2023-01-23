2b1b016c0b85e7cf23661017f88fdda152041b34
NikaN-A <nika377344@gmail.com>
2023-01-23 17:51:30 +0200

    Used the correct main icon

--- src/Search.js
+++ src/Search.js
@@ -15,7 +15,7 @@ export default function Search(props) {
       city: response.data.name,
       description: response.data.weather[0].description,
       humidity: response.data.main.humidity,
-      iconUrl: "https://ssl.gstatic.com/onebox/weather/64/cloudy.png",
+      iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
       date: new Date(response.data.dt * 1000),
     });
   }
